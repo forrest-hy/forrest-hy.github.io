acbf09daa8f5687944e6383951d2e74e5baee548
forrest-hy <115953152+forrest-hy@users.noreply.github.com>
2022-10-31 15:55:56 -0400

    Timer app
    A primitive timer web app for Chinese debate events

---
+++ scripts/cfTimer.js
@@ -0,0 +1,114 @@
+const proTimer = document.querySelector("#proTimer")
+const conTimer = document.querySelector("#conTimer")
+const originalTime = parseInt(document.querySelector("body").className)
+let finished = 2
+let remind_30 = new Audio("../audio/30s.mp3")
+let endSound = new Audio('../audio/end.mp3')
+
+const timerPrototype = {
+    decrementTimer() {
+        this.remainingTime--
+        if (this.remainingTime === 0) {
+            proNextTurn = !proNextTurn
+            conNextTurn = !conNextTurn
+            finished--
+            this.hasRemainingTime = false
+            playEndSound()
+            this.stopTimer()
+            if (this.otherTimer.remainingTime !== 0) {
+                this.otherTimer.resumeTimer()
+            }
+        } else if (this.remainingTime < 0) {
+            this.remainingTime = 0
+        } else if (this.remainingTime === 30) {
+            play30sSound()
+        }
+        this.timer.textContent = timeToString(this.remainingTime)
+    },
+
+    resumeTimer() {
+        var t = this
+        this.countdownHandle = setInterval(function () {t.decrementTimer()}, 1000)
+    },
+
+    stopTimer() {
+        clearInterval(this.countdownHandle)
+    },
+
+    resetTimer() {
+        this.stopTimer()
+        this.remainingTime = this.originalTime
+        this.hasRemainingTime = true
+        this.timer.textContent = timeToString(this.originalTime)
+    },
+}
+
+function decrement(myTimer) {
+    myTimer.remainingTime--
+    if (myTimer.remainingTime === 0) {
+        playEndSound()
+        myTimer.stopTimer()
+    } else if (myTimer.remainingTime === 30) {
+        play30sSound()
+    }
+    myTimer.timer.textContent = timeToString(myTimer.remainingTime)
+}
+
+function Timer (timerWrap, time, other) {
+    this.timer = timerWrap
+    this.originalTime = time
+    this.remainingTime = time
+    this.otherTimer = other
+    this.hasRemainingTime = true
+}
+
+Object.assign(Timer.prototype, timerPrototype)
+const proTimerObject = new Timer(proTimer, originalTime)
+const conTimerObject = new Timer(conTimer, originalTime, proTimerObject)
+proTimerObject.otherTimer = conTimerObject
+
+
+let proNextTurn = true
+let conNextTurn = false
+
+function timeToString (t) {
+    let seconds =  t % 60;
+    let minutes = (t - seconds) / 60;
+    return String(minutes).padStart(2, "0") + ":" + String(seconds).padStart(2, "0")
+}
+
+function handleButton() {
+    if (finished === 0) {
+    } else if (proNextTurn && proTimerObject.hasRemainingTime) {
+        proNextTurn = false
+        conNextTurn = true
+        conTimerObject.stopTimer()
+        proTimerObject.resumeTimer();
+    } else if (conNextTurn && conTimerObject.hasRemainingTime) {
+        conNextTurn = false
+        proNextTurn = true
+        proTimerObject.stopTimer()
+        conTimerObject.resumeTimer();
+    }
+}
+
+function resetBoth() {
+    proTimerObject.resetTimer()
+    conTimerObject.resetTimer()
+    proNextTurn = true
+    conNextTurn = false
+    finished = 2
+}
+
+function stopBoth() {
+    proTimerObject.stopTimer()
+    conTimerObject.stopTimer()
+}
+
+function playEndSound() {
+    endSound.play()
+}
+
+function play30sSound() {
+    remind_30.play()
+}
---
+++ scripts/timer.js
@@ -0,0 +1,61 @@
+const timer = document.querySelector(".timer")
+let time = parseInt(document.querySelector("body").className)
+let startButton = document.querySelector("#startButton")
+let remind_30 = new Audio("../audio/30s.mp3")
+let endSound = new Audio('../audio/end.mp3')
+
+const originalTime = time
+let isRunning = false
+
+function timeToString (t) {
+    let seconds =  t % 60;
+    let minutes = (t - seconds) / 60;
+    return String(minutes).padStart(2, "0") + ":" + String(seconds).padStart(2, "0")
+}
+
+function decrementTimer() {
+    time--
+    if (time === 0) {
+        playEndSound()
+        stopTimer()
+    } else if (time === 30) {
+        play30sSound()
+    }
+    timer.textContent = timeToString(time)
+}
+
+function handleButton() {
+    if (isRunning) {
+        stopTimer()
+    } else {
+        resumeTimer()
+    }
+}
+
+function stopTimer() {
+    isRunning = false
+    startButton.textContent="开始"
+    clearInterval(countdownHandle)
+}
+
+function resumeTimer(){
+    if (time > 0) {
+        isRunning = true
+        startButton.textContent="暂停"
+        countdownHandle = setInterval(function () {decrementTimer()}, 1000)
+    }
+}
+
+function resetTimer() {
+    stopTimer()
+    time = originalTime
+    timer.textContent = timeToString(time)
+}
+
+function playEndSound() {
+    endSound.play()
+}
+
+function play30sSound() {
+    remind_30.play()
+}
